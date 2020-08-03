1bda521a08b18343b8751b0e0e4675e63a1f7a9e
Eder Andrew <ederandrew0028@gmail.com>
2020-08-02 21:45:20 -0300

    Corrigindo api do youtube

--- src/api/youtube/youtube.api.js
+++ src/api/youtube/youtube.api.js
@@ -1,10 +1,27 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',
+    headers: {
+        Authorization: 'Bearer 32463881764-lovdplq8m86ek2i1qcv3b895s4cqipn5.apps.googleusercontent.com',
+        Accept: 'application/json'
+    }
+})
 export const Culto_louvor = async (callback) => {
-    await fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=date&q=Igreja%20Batista%20Regular%20parque%20ipe&type=video&key=AIzaSyDze8u0YmpbYz0ad6Wn2W7d8hzkVmDSdCI")
-            .then(response=>response.json())
-            .then(callback)
-            .catch(error=>{
-                console.log(error.message);
-            });
+    await axios.get("/search", {
+        params: {
+            part: 'snippet',
+            maxResults: 10,
+            order: 'date',
+            q: 'Igreja Batista Regular parque ipe',
+            type:'video',
+            key: 'AIzaSyDze8u0YmpbYz0ad6Wn2W7d8hzkVmDSdCI'
+        }
+    }).then(response=>response.json())
+      .then(callback)
+      .catch(error=>{
+        console.log(error.message);
+      });
 }
 
 export const Oracao_meio = async (callback) => {
